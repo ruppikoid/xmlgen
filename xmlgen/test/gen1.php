a3448e5c42af789ced2bc06ceb9e0b5a054a8b5c
Unknown <sasha9172@gmail.com>
2018-04-29 19:11:23 +0300

    gen
    change gen.php
    add folder test

---
+++ xmlgen/test/gen1.php
@@ -0,0 +1,24 @@
+<?php
+ // if (isset($_POST['gen1'])) {
+ // 	$xml = new DOMDocument("1.0", "utf-8");
+ //
+	// $xml->load('books.xml');
+ //
+	// $rootTag = $xml->getElementsByTagName("root")->item(0);
+ //
+	// $dataTag = $xml->createElement("data");
+ //    $name = $xml->createElement("name",$_POST['name']);
+ //    $city = $xml->createElement("city",$_POST['city']);
+ //
+ //    $dataTag->appendChild($name);
+ //    $dataTag->appendChild($city);
+ //
+ //  $rootTag->appendChild($dataTag);
+ //
+ //  $xml->save('test.xml');
+ //
+ //  header("Location: test.php");
+ //  exit;
+ //
+ // }
+?>
---
+++ xmlgen/test/test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>test</title>
+  </head>
+  <body>
+    <form action="gen1.php" method="post">
+      Информация о человеке
+      <p>Имя:<input type="text" name="name" value=""></p>
+      <p>Город:<input type="text" name="city" value=""></p>
+      <input type="submit" name="gen1" value="Добавить">
+    </form>
+  </body>
+</html>
